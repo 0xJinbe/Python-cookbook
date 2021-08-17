06d88f81fe3b06e14ae19f20a046535dcb1fb5fe
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-08-17 20:24:11 -0300

    functions part 1- args kwargs

---
+++ functions.py
@@ -0,0 +1,41 @@
+#Arbitrary Arguments, *args
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
+
+
+print('---------------------------')
+def my_function(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+my_function(child1 = "Emil", child2 = "Tobias", child3 = "Linus")
+
+#Arbitrary Keyword Arguments, **kwargs
+
+def my_function(**kid):
+  print("His last name is " + kid["lname"])
+
+my_function(fname = "Tobias", lname = "Refsnes")
+
+print('--------------')
+#parameter value
+
+def my_function(country = "Norway"):
+  print("I am from " + country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+
+#list as argument
+
+def my_function(food):
+  for x in food:
+    print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
