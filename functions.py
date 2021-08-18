333be9916baa0e492b473edafc0471e46d5e8404
Claudio W. Germano Jr <80705963+claudio-germano@users.noreply.github.com>
2021-08-18 12:43:14 -0300

    fucntions end file

--- functions.py
+++ functions.py
@@ -39,3 +39,12 @@ def my_function(food):
 fruits = ["apple", "banana", "cherry"]
 
 my_function(fruits)
+
+#return functions
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
